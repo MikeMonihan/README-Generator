aa37b9ebc780b99d53415634711c765549358b2f
Michael Monihan <37195785+MikeMonihan@users.noreply.github.com>
2022-02-16 19:17:01 -0500

    Added questions to generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,143 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
+const genMark = require('./utils/generateMarkdown');
+var a = "a";
 
-// TODO: Create an array of questions for user input
-const questions = [];
+console.log('Welcome to the GitHub Project README Generator!');
+console.log(typeof genMark.renderLicenseBadge);
+
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title for your project: ',
+      
+},
+{
+    type: 'input',
+    name:'description',
+    message: 'Enter the description for your project: ',
+},
+{
+    type: 'checkbox',
+    name: 'tOC',
+    message: 'Enter a table of contents: ',
+    choices: [
+    {
+        key:'0',
+        name: 'Installation',
+        value: 'Installation',
+    },
+    {
+        key:'1',
+        name: 'Usage',
+        value: 'Usage',
+    },
+    {
+        key:'2',
+        name: 'Credits',
+        value: 'Credits',
+    },
+    {
+        key:'3',
+        name: 'License',
+        value: 'License',
+    },
+    ],
+},
+{
+    type: 'input',
+    name:'installInfo',
+    message: 'Enter installation information: ',
+},
+{
+    type: 'input',
+    name:'usageInfo',
+    message: 'Enter usage information: ',
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Enter the license used: ',
+    choices: [
+    {
+        key:'0',
+        name: 'GNU AGPLv3',
+        value: 'GNU AGPLv3',
+    },
+    {
+        key:'1',
+        name: 'GNU GPLv3',
+        value: 'GNU GPLv3',
+    },
+    {
+        key:'2',
+        name: 'GNU LGPLv3',
+        value: 'GNU LGPLv3',
+    },
+    {
+        key:'3',
+        name: 'Mozilla Public License 2.0',
+        value: 'Mozilla Public License 2.0',
+    },
+    {
+        key:'4',
+        name: 'Apache License 2.0',
+        value: 'Apache License 2.0',
+    },
+    {
+        key:'5',
+        name: 'MIT License',
+        value: 'MIT License',
+    },
+    {
+        key:'6',
+        name: 'Boost Software License 1.0',
+        value: 'Boost Software License 1.0',
+    },
+    {
+        key:'7',
+        name: 'The Unlicense',
+        value: 'The Unlicense',
+    },
+    ],
+},
+{
+    type: 'input',
+    name:'contrib',
+    message: 'Enter any contribution guidelines: ',
+},
+{
+    type: 'input',
+    name:'testInst',
+    message: 'Enter any test instructions: ',
+},
+{
+    type: 'input',
+    name:'gitHubUser',
+    message: 'Please enter your GitHub username: ',
+},
+{
+    type: 'input',
+    name:'email',
+    message: 'Please enter your email: ',
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    inquirer.prompt(questions).then((answers) => {
+        console.log('\nOrder receipt:');
+        console.log(JSON.stringify(answers, null, '  '));
+      });
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {writeToFile();}
 
+//module.exports = index;
 // Function call to initialize app
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,11 @@
+//const index = require('Develop/index.js');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
